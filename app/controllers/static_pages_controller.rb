ee779170de27184e9ff1d8c086be9b26b9a13fa6
chris-clifton <cliftoncpx@gmail.com>
2019-01-12 13:22:44 -0500

    Fixed StaticPages controller

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,7 +1,9 @@
 class StaticPagesController < ApplicationController
   def home
+    render html: "this should be the home page"
   end
 
   def help
+    render html: "this should be the help page"
   end
 end
