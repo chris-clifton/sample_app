25afcd59adf1eda8c141dc7e0651e32364b050fd
chris-clifton <cliftoncpx@gmail.com>
2019-01-13 15:18:29 -0500

    Finish static pages

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,15 +1,13 @@
 class StaticPagesController < ApplicationController
   def home
-    @page_title = 'Home'
-    #render html: "this should be the home page"
   end
 
   def help
-    @page_title = 'Help'
-    # render html: "this should be the help page"
   end
 
-  def about
-     @page_title = 'About'
+  def about    
+  end
+
+  def contact
   end
 end
