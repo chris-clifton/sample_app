e874e244bb6c473c8bdc17136083887b3cc8450a
chris-clifton <cliftoncpx@gmail.com>
2019-01-12 23:42:01 -0500

    Controller and Views lesson

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,9 +1,11 @@
 class StaticPagesController < ApplicationController
   def home
-    render html: "this should be the home page"
+    @page_title = 'Home'
+    #render html: "this should be the home page"
   end
 
   def help
-    render html: "this should be the help page"
+    @page_title = 'Help'
+    # render html: "this should be the help page"
   end
 end
