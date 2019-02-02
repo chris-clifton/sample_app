92101969b24f2f61a19ede7c82a9a6689b638950
chris-clifton <cliftoncpx@gmail.com>
2019-02-02 17:17:15 -0500

    testing password reset links on heroku

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -7,12 +7,11 @@ class UserMailer < ApplicationMailer
   #
   def account_activation(user)
     @user = user
-    mail to: user.email, subject: "Account Activation"
+    mail to: user.email, subject: "Sample App: Account Activation"
   end
 
-  def password_reset
-    @greeting = "Hi"
-
-    mail to: "to@example.org"
+  def password_reset(user)
+    @user = user
+    mail to: user.email, subject: "Sample App: Password Reset"
   end
 end
