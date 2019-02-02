8ea4ec9e5dae06ade420c1537311ac775f77d7bd
chris-clifton <cliftoncpx@gmail.com>
2019-02-02 13:29:27 -0500

    fixed production.rb syntax error

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -96,8 +96,8 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     :address        =>  'smtp.sendgrid.net',
-    :port           =>  '587'
-    :authentication =>  :plain;
+    :port           =>  '587',
+    :authentication =>  :plain,
     :user_name      =>  ENV['SENDGRID_USERNAME'],
     :password       =>  ENV['SENDGRID_PASSWORD'],
     :domain         =>  'heroku.com',
